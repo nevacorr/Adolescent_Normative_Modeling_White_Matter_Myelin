1f9474af3e11a3f231fac9348c13d02b7901f7e3
Neva <nevao@uw.edu>
2024-11-13 12:56:07 -0800

    removed unused function

--- Load_Genz_Data_WWM_MPF.py
+++ Load_Genz_Data_WWM_MPF.py
@@ -7,34 +7,6 @@ import pandas as pd
 from load_raw_data_WM_MPF import load_raw_data_wm_mpf
 from load_genz_tract_profile_data import load_genz_tract_profile_data
 
-def load_genz_data_wm_mpf_v2(struct_var, path, braindatafilename_v2, demographics_filename):
-
-    visit = 2
-    # get brain data
-    brain_data = load_raw_data_wm_mpf(struct_var, visit, path, braindatafilename_v2)
-
-    # convert subject numbers to integers
-    brain_data['Subject'] = brain_data['Subject'].str.replace('sub-genz', '', regex=False).astype(int)
-
-    # get demographic data
-    demo_data = pd.read_csv(f'{path}/{demographics_filename}')
-    demo_data = demo_data[demo_data['visit']==visit]
-    demo_to_keep = ['subject', 'gender', 'agemonths', 'agedays', 'agegroup']
-    cols_to_drop = [item for item in demo_data.columns if item not in demo_to_keep]
-    demo_data.drop(columns=cols_to_drop, inplace=True)
-    demo_data.dropna(inplace=True, ignore_index=True)
-    columns_to_convert = demo_data.columns.difference(['subject'])
-    demo_data[columns_to_convert] = demo_data[columns_to_convert].astype('int64')
-
-    # merge demo data with brain data
-    brain_data.rename(columns={'Subject': 'subject'}, inplace=True)
-    all_data = pd.merge(demo_data, brain_data, how='right', on = 'subject')
-    all_data.drop(columns=['Age'], inplace=True)
-
-    all_data.rename(columns={'subject': 'participant_id', 'gender':'sex', 'agegroup': 'age'}, inplace=True)
-
-    return all_data
-
 def load_genz_data_wm_mpf_v1(struct_var, path, braindatafilename_v1, braindatafilename_v2,
                              demographics_filename):
 
