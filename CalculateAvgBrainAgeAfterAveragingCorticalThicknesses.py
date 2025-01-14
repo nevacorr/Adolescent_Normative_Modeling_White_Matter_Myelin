52ae29808e517ee75cbcc8d45d154088a207cbc5
Neva <nevao@uw.edu>
2025-01-13 17:03:04 -0800

    Initial commit. Code copied from cortical thickness analysis project.

---
+++ CalculateAvgBrainAgeAfterAveragingCorticalThicknesses.py
@@ -0,0 +1,134 @@
+#####
+# This program calculates brain age acceleration based on the adolescent data. It averages cortical thickness across
+# all brain regions. It fits a model on the precovid data and evaluates the model on the post-covid data. It
+# includes an option to use bootstrapping for confidence interval calculation.
+# Author: Neva M. Corrigan
+# Date: 21 February, 2024
+######
+
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from Utility_Functions import plot_age_acceleration
+from scipy.stats import percentileofscore
+from Load_Genz_Data import load_genz_data
+from plot_num_subjs import plot_num_subjs
+from calculate_avg_brain_age_acc import calculate_avg_brain_age_acceleration_make_model
+from calculate_avg_brain_age_acc import calculate_avg_brain_age_acceleration_apply_model
+from calculate_avg_brain_age_acceleration_apply_model_bootstrap import calculate_avg_brain_age_acceleration_apply_model_bootstrap
+
+struct_var = 'cortthick'
+show_plots = 0  #set to 1 to show training and test data y vs yhat and spline fit plots. Set to 0 to save to file.
+show_nsubject_plots = 0 #set to 1 to show number of subjects at each age and sex in analysis
+spline_order = 1
+spline_knots = 2
+perform_train_test_split_precovid = 1  # flag indicating whether training set was split into train and validation set
+filepath = os.getcwd()
+subjects_to_exclude = [525]  # subjects to exclude from analysis. Subject 525 had an incidental finding
+calc_CI_age_acc_bootstrap = 0  # specify whether to run bootstrap analysis for CI calculation
+nbootstrap = 1000  #specify number of bootstraps
+
+# Turn off interactive mode, don't show plots unless plt.show() is specified
+plt.ioff()
+
+# Load visit 1 data
+visit=1
+brain_good, all_data, roi_ids = load_genz_data(struct_var, visit, filepath)
+
+# Remove subject 525 who has an incidental finding
+all_data = all_data[~all_data['participant_id'].isin(subjects_to_exclude)]
+
+# Read in file of subjects in post-COVID test set
+fname='{}/visit2_all_subjects_used_in_test_set_cortthick.txt'.format(filepath, struct_var)
+subjects_test = pd.read_csv(fname, header=None)
+
+# Exclude subjects at 9, 11 and 13 who are in test set from dataframe of visit 1 data
+all_data = all_data[~all_data['participant_id'].isin(subjects_test[0])]
+
+# Replace gender with gender=0 female gender =1 male
+all_data.loc[all_data['sex']==2, 'sex'] = 0
+
+# Plot number of subjects of each gender by age who are included in training data set
+if show_nsubject_plots:
+    plot_num_subjs(all_data, 'Subjects by Age with Pre-COVID Data\nUsed to Create Model\n'
+                   '(Total N=' + str(all_data.shape[0]) + ')', struct_var, 'pre-covid_norm_model', filepath)
+
+# Drop amy rows with any missing values
+all_data = all_data.dropna()
+all_data.reset_index(inplace=True, drop=True)
+
+# If validation data was not used in model creation, exclude these subjects from dataframe
+if perform_train_test_split_precovid == 1:
+    fname_train = f'{filepath}/train_subjects_excludes_validation.csv'
+    subjects_train = pd.read_csv(fname_train, header=None)
+    subjects_train = subjects_train[0].tolist()
+    all_data = all_data[all_data['participant_id'].isin(subjects_train)]
+
+# Separate the brain features (response variables) and predictors (age, gender) in to separate dataframes
+all_data_features_orig = all_data.loc[:,roi_ids]
+all_data_covariates = all_data[['age', 'agedays', 'sex']]
+
+# Average cortical thickness across all regions for each subject
+all_data_features = all_data_features_orig.mean(axis=1).to_frame()
+all_data_features.rename(columns={0:'avgcortthick'},  inplace=True)
+
+# Create model for when cortthick is averaged across the entire brain
+model_dir, agemin, agemax = calculate_avg_brain_age_acceleration_make_model('allreg',
+                        all_data, all_data_covariates, all_data_features, struct_var, show_plots,
+                        spline_order, spline_knots, filepath)
+
+# Specify visit number
+visit = 2
+# Load all brain and behavior data for visit 2
+brain_good, all_datav2, roi_ids = load_genz_data(struct_var, visit, filepath)
+
+# Load test subject numbeers
+fname = '{}/visit2_all_subjects_used_in_test_set_{}.txt'.format(filepath, struct_var)
+my_file = open(fname, 'r')
+test_subjects_txt = my_file.read()
+test_subjects = test_subjects_txt.split("\n")
+my_file.close()
+while ("" in test_subjects):
+    test_subjects.remove("")
+test_subjects = [int(i) for i in test_subjects]
+
+# Create a dataframe with just test subject data
+all_datav2 = all_datav2[all_datav2['participant_id'].isin(test_subjects)]
+
+# Recode gender as 0=female 1=male
+all_datav2.loc[all_datav2['sex'] == 2, 'sex'] = 0
+
+# Show number of subjects by gender and age
+if show_nsubject_plots:
+    plot_num_subjs(all_datav2, 'Subjects with Post-COVID Data\nEvaluated by Model\n'
+                   + ' (Total N=' + str(all_datav2.shape[0]) + ')', struct_var, 'post-covid_allsubj', filepath)
+
+# Reset indices
+all_datav2.reset_index(inplace=True, drop=True)
+
+# Calculate age acceleration
+agediff_female, agediff_male = calculate_avg_brain_age_acceleration_apply_model(roi_ids, 'allreg', all_datav2,
+                                                 struct_var, show_plots, model_dir, spline_order, spline_knots, filepath,
+                                                 agemin, agemax)
+
+# If calculate bootstrap, run analysis repeatedly for each bootstrap and calculate confidence intervals
+if calc_CI_age_acc_bootstrap:
+
+    mean_agediff_boot_f, mean_agediff_boot_m = calculate_avg_brain_age_acceleration_apply_model_bootstrap(roi_ids,
+                                                    all_datav2, struct_var, spline_order, spline_knots,
+                                                    filepath, agemin, agemax, nbootstrap)
+
+    ageacc_from_bootstraps = {}
+    ageacc_from_bootstraps['female'] = mean_agediff_boot_f
+    ageacc_from_bootstraps['male'] = mean_agediff_boot_m
+
+    # Write age acceleration from bootstrapping to file
+    with open(f"{filepath}/ageacceleration_dictionary {nbootstrap} bootstraps.txt", 'w') as f:
+        for key, value in ageacc_from_bootstraps.items():
+            f.write('%s:%s\n' % (key, value))
+
+# Show plot of age acceleration for each gender
+plot_age_acceleration(filepath, nbootstrap, agediff_female, agediff_male)
+
+plt.show()
---
+++ calculate_avg_brain_age_acc.py
@@ -0,0 +1,212 @@
+import pandas as pd
+import os
+import shutil
+from pcntoolkit.normative import estimate, predict
+from Utility_Functions import create_design_matrix, plot_data_with_spline, create_dummy_design_matrix
+from Utility_Functions import makenewdir, movefiles, write_list_of_lists, read_list_of_lists
+from normative_edited import predict
+from calculate_brain_age_acceleration import calculate_age_acceleration
+from plot_and_compute_zdistributions import plot_and_compute_zcores_by_gender
+import matplotlib.pyplot as plt
+import random
+
+def calculate_avg_brain_age_acceleration_make_model(desc_string, all_data, all_data_covariates, all_data_features,
+                                                        struct_var, show_plots, spline_order, spline_knots, filepath):
+
+    # Create output directories
+    makenewdir('{}/avgct_{}/'.format(filepath, desc_string))
+    makenewdir('{}/avgct_{}/{}'.format(filepath, desc_string, struct_var))
+    makenewdir('{}/avgct_{}/{}/plots'.format(filepath, desc_string, struct_var))
+    makenewdir('{}/avgct_{}/{}/ROI_models'.format(filepath, desc_string, struct_var))
+    makenewdir('{}/avgct_{}/{}/covariate_files'.format(filepath, desc_string, struct_var))
+    makenewdir('{}/avgct_{}/{}/response_files'.format(filepath, desc_string, struct_var))
+
+    # Save covariates and feature dataframes to new variables
+    X_train = all_data_covariates.copy()
+    y_train = all_data_features.copy()
+
+    # Determine min and max age of subjects in dataset
+    agemin=X_train['agedays'].min()
+    agemax=X_train['agedays'].max()
+
+    # Save the subject numbers for the training set
+    s_index_train = X_train.index.values
+    subjects_train = all_data.loc[s_index_train, 'participant_id'].values
+
+    # Drop the age column because we want to use agedays as a predictor
+    X_train.drop(columns=['age'], inplace=True)
+
+    # Reset the indices in the train data set
+    X_train.reset_index(drop=True, inplace=True)
+    y_train.reset_index(drop=True, inplace=True)
+
+    ##########
+    # Set up output directories. In this case, there is only one for average cortical thickness computations.
+    ##########
+    for c in y_train.columns:
+        y_train[c].to_csv(f'{filepath}/resp_tr_'+c+'.txt', header=False, index=False)
+        X_train.to_csv(f'{filepath}/cov_tr.txt', sep='\t', header=False, index=False)
+        y_train.to_csv(f'{filepath}/resp_tr.txt', sep='\t', header=False, index=False)
+
+    for i in ['avgcortthick']:
+        roidirname = '{}/avgct_{}/{}/ROI_models/{}'.format(filepath, desc_string, struct_var, i)
+        makenewdir(roidirname)
+        resp_tr_filename = "{}/resp_tr_{}.txt".format(filepath, i)
+        resp_tr_filepath = roidirname + '/resp_tr.txt'
+        shutil.copyfile(resp_tr_filename, resp_tr_filepath)
+        cov_tr_filepath = roidirname + '/cov_tr.txt'
+        shutil.copyfile(f"{filepath}/cov_tr.txt", cov_tr_filepath)
+
+    movefiles("{}/resp_*.txt", "{}/avgct_{}/{}/response_files/".format(filepath, filepath, desc_string, struct_var))
+    movefiles("{}/cov_t*.txt", "{}/avgct_{}/{}/covariate_files/".format(filepath, filepath, desc_string, struct_var))
+
+    # Define path is where ROI_models folders are located
+    data_dir='{}/avgct_{}/{}/ROI_models/'.format(filepath, desc_string, struct_var)
+
+    # Create Design Matrix and add in spline basis and intercept
+    create_design_matrix('train', agemin, agemax, spline_order, spline_knots, ['avgcortthick'], data_dir)
+
+    # Estimate the normative model using a for loop to iterate over brain regions. The estimate function uses a few specific arguments that are worth commenting on:
+    # ●alg=‘blr’: specifies we should use BLR. See Table1 for other available algorithms
+    # ●optimizer=‘powell’:usePowell’s derivative-free optimization method(faster in this case than L-BFGS)
+    # ●savemodel=True: do not write out the final estimated model to disk
+    # ●saveoutput=False: return the outputs directly rather than writing them to disk
+    # ●standardize=False: do not standardize the covariates or response variable
+
+    # Loop through ROIs. In this case there is only one roi: average cortical thickness
+
+    for roi in ['avgcortthick']:
+        print('Running ROI:', roi)
+        roi_dir=os.path.join(data_dir, roi)
+        model_dir = os.path.join(data_dir, roi, 'Models')
+        os.chdir(roi_dir)
+
+        # Configure the covariates to use. Change *_bspline_* to *_int_*
+        cov_file_tr=os.path.join(roi_dir, 'cov_bspline_tr.txt')
+
+        # Load train & test response files
+        resp_file_tr=os.path.join(roi_dir, 'resp_tr.txt')
+
+        # Run a basic model on the training dataset and store the predicted response (yhat_tr), the variance of the
+        # predicted response (s2_tr), the model parameters (nm), the  Zscores for the train data, and other
+        # Various metrics (metrics_tr)
+        yhat_tr, s2_tr, nm_tr, Z_tr, metrics_tr = estimate(cov_file_tr, resp_file_tr, testresp=resp_file_tr,
+                                                    testcov=cov_file_tr, alg='blr', optimizer='powell',
+                                                    savemodel=True, saveoutput=False,standardize=False)
+
+        #create dummy design matrices
+        dummy_cov_file_path_female, dummy_cov_file_path_male = \
+            create_dummy_design_matrix(struct_var, agemin, agemax, cov_file_tr, spline_order, spline_knots, filepath)
+
+        #compute splines and superimpose on data. Show on screen or save to file depending on show_plots value.
+        plot_data_with_spline('Training Data', struct_var, cov_file_tr, resp_file_tr, dummy_cov_file_path_female,
+                              dummy_cov_file_path_male, model_dir, roi, show_plots, filepath)
+
+    plt.show()
+
+    return data_dir, agemin, agemax
+
+def calculate_avg_brain_age_acceleration_apply_model(roi_ids, desc_string, all_datav2, struct_var, show_plots, model_dir, spline_order,
+                                                     spline_knots, working_dir, agemin, agemax):
+
+    #############################  Apply Normative Model to Post-COVID Data ####################
+
+    # separate the brain features (response variables) and predictors (age, gender) in to separate dataframes
+    all_datav2_features = all_datav2.loc[:, roi_ids]
+    all_datav2_covariates = all_datav2[['agedays', 'sex']]
+
+    # average cortical thickness across all regions for each subject
+    all_datav2_features = all_datav2_features.mean(axis=1).to_frame()
+    all_datav2_features.rename(columns={0: 'avgcortthick'}, inplace=True)
+
+    #make file diretories for output
+    makenewdir('{}/avgct_{}_predict_files/'.format(working_dir, desc_string))
+    makenewdir('{}/avgct_{}_predict_files/{}'.format(working_dir, desc_string, struct_var))
+    makenewdir('{}/avgct_{}_predict_files/{}/plots'.format(working_dir, desc_string, struct_var))
+    makenewdir('{}/avgct_{}_predict_files/{}/ROI_models'.format(working_dir, desc_string, struct_var))
+    makenewdir('{}/avgct_{}_predict_files/{}/covariate_files'.format(working_dir, desc_string, struct_var))
+    makenewdir('{}/avgct_{}_predict_files/{}/response_files'.format(working_dir, desc_string, struct_var))
+
+    roi_ids = ['avgcortthick']
+
+    #specify paths
+    out_dir = ('{}/avgct_{}_predict_files/{}/ROI_models/'
+               .format(working_dir, desc_string, struct_var))
+    #  this path is where ROI_models folders are located
+    predict_files_dir = ('{}/avgct_{}_predict_files/{}/ROI_models/'
+                .format(working_dir, desc_string, struct_var))
+
+    ##########
+    # Create output directories for each region and place covariate and response files for that region in  each directory
+    ##########
+    for c in all_datav2_features.columns:
+        all_datav2_features[c].to_csv(f'{working_dir}/resp_te_'+c+'.txt', header=False, index=False)
+        all_datav2_covariates.to_csv(f'{working_dir}/cov_te.txt', sep='\t', header=False, index=False)
+        all_datav2_features.to_csv(f'{working_dir}/resp_te.txt', sep='\t', header=False, index=False)
+
+    for i in ['avgcortthick']:
+        roidirname = ('{}/avgct_{}_predict_files/{}/ROI_models/{}'
+                      .format(working_dir, desc_string, struct_var, i))
+        makenewdir(roidirname)
+        resp_te_filename = "{}/resp_te_{}.txt".format(working_dir, i)
+        resp_te_filepath = roidirname + '/resp_te.txt'
+        shutil.copyfile(resp_te_filename, resp_te_filepath)
+        cov_te_filepath = roidirname + '/cov_te.txt'
+        shutil.copyfile(f"{working_dir}/cov_te.txt", cov_te_filepath)
+
+    movefiles("{}/resp_*.txt", "{}/avgct_{}_predict_files/{}/response_files/"
+              .format(working_dir, working_dir, desc_string, struct_var))
+    movefiles("{}/cov_t*.txt", "{}/avgct_{}_predict_files/{}/covariate_files/"
+              .format(working_dir, working_dir, desc_string, struct_var))
+
+    # Create Design Matrix and add in spline basis and intercept
+    create_design_matrix('test', agemin, agemax, spline_order, spline_knots, roi_ids, out_dir)
+
+    # Create dataframe to store Zscores
+    Z_time2 = pd.DataFrame()
+    Z_time2['participant_id'] = all_datav2['participant_id'].copy()
+    Z_time2.reset_index(inplace=True, drop = True)
+
+    ####Make Predictions of Brain Structural Measures Post-Covid based on Pre-Covid Normative Model
+
+    #create design matrices for all regions and save files in respective directories
+    create_design_matrix('test', agemin, agemax, spline_order, spline_knots, roi_ids, predict_files_dir)
+
+    for roi in roi_ids:
+        print('Running ROI:', roi)
+        roi_dir=os.path.join(predict_files_dir, roi)
+        model_dir = os.path.join(model_dir, roi, 'Models')
+        os.chdir(roi_dir)
+
+        # configure the covariates to use.
+        cov_file_te=os.path.join(roi_dir, 'cov_bspline_te.txt')
+
+        # load test response files
+        resp_file_te=os.path.join(roi_dir, 'resp_te.txt')
+
+        # make predictions
+        yhat_te, s2_te, Z = predict(cov_file_te, respfile=resp_file_te, alg='blr', model_path=model_dir)
+
+        Z_time2[roi] = Z
+
+        #create dummy design matrices
+        dummy_cov_file_path_female, dummy_cov_file_path_male = \
+            create_dummy_design_matrix(struct_var, agemin, agemax, cov_file_te, spline_order, spline_knots, working_dir)
+
+        #compute splines and superimpose on data. Show on screen or save to file depending on show_plots value.
+        plot_data_with_spline('Test Data', struct_var, cov_file_te, resp_file_te, dummy_cov_file_path_female,
+                              dummy_cov_file_path_male, model_dir, roi, show_plots, working_dir)
+
+
+        #calculate brain age acceleration for each region
+        mean_agediff_f, mean_agediff_m = calculate_age_acceleration(struct_var, roi_dir, yhat_te, model_dir, roi,
+                                                        dummy_cov_file_path_female, dummy_cov_file_path_male, plotgap=1)
+
+
+    Z_time2.to_csv('{}/avgct_{}_predict_files/{}/Z_scores_by_region_postcovid_testset_avgct.txt'
+                                .format(working_dir, desc_string, struct_var), index=False)
+
+    plot_and_compute_zcores_by_gender(Z_time2, struct_var, roi_ids, working_dir)
+    plt.show()
+
+    return mean_agediff_f, mean_agediff_m
