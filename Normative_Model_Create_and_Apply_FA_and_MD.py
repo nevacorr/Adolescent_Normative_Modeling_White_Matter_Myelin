ab49342de193ba60e61a1751082893eb2517dae4
Neva <nevao@uw.edu>
2025-01-21 13:20:16 -0800

    Add missing subejcts for MD and FA analysis

--- Normative_Model_Create_and_Apply_FA_and_MD.py
+++ Normative_Model_Create_and_Apply_FA_and_MD.py
@@ -14,7 +14,7 @@ from make_and_apply_normative_model_fa_md import make_and_apply_normative_model_
 from compute_df_correlations import compute_df_correlations
 
 struct_var = 'fa_and_md_and_mpf'
-n_splits = 10   #Number of train/test splits
+n_splits = 100   #Number of train/test splits
 show_plots = 0          #set to 1 to show training and test data ymvs yhat and spline fit plots.
 show_nsubject_plots = 0 #set to 1 to plot number of subjects used in analysis, for each age and gender
 spline_order = 1        # order of spline to use for model
@@ -28,11 +28,10 @@ md_visit1_datafile = 'genz_tract_profile_data/genzMD_tractProfiles_visit1.csv'
 md_visit2_datafile = 'genz_tract_profile_data/genzMD_tractProfiles_visit2.csv'
 mpf_visit1_datafile = 'tableGenzVisit1_allTracts_Oct22.csv'
 mpf_visit2_datafile = 'tableGenzVisit2_allTracts_Oct22.csv'
-subjects_to_exclude_time1 = []  # subjects to exclude for FA and MD
-subjects_to_exclude_time2 = []  # subjects to exclude for FA and MD
-mpf_subjects_to_exclude_time1 = [106, 107, 111, 121, 122, 126, 127, 208, 209, 210, 211, 214, 215, 221, 226, 309, 323, 335,
-                             405, 418, 421, 423, 524]
-mpf_subjects_to_exclude_time2 = [105, 117, 119, 201, 209, 215, 301, 306, 319, 321, 325, 406, 418, 421, 515, 527]
+subjects_to_exclude_time1 = [525]  # subjects to exclude for FA and MD
+subjects_to_exclude_time2 = [525]  # subjects to exclude for FA and MD
+mpf_subjects_to_exclude_time1 = []#[106, 107, 111, 121, 122, 126, 127, 208, 209, 210, 211, 214, 215, 221, 226, 309, 323, 335,405, 418, 421, 423, 524]
+mpf_subjects_to_exclude_time2 = [] #[105, 117, 119, 201, 209, 215, 301, 306, 319, 321, 325, 406, 418, 421, 515, 527]
 
 file_with_demographics = 'Adol_CortThick_data.csv'
 
@@ -51,6 +50,7 @@ if run_make_norm_model:
 
     plt.show(block=False)
 
+    tmp = Z_time2_fa.groupby(by=['participant_id'])
     Z_time2_fa = Z_time2_fa.groupby(by=['participant_id']).mean().drop(columns=['split'])
     Z_time2_md = Z_time2_md.groupby(by=['participant_id']).mean().drop(columns=['split'])
     Z_time2_mpf = Z_time2_mpf.groupby(by=['participant_id']).mean().drop(columns=['split'])
